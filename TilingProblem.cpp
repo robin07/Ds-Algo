fc5ffb32b83e627b335151291bcbc5e611d70deb
robin07 <robbiesingh.singh84@gmail.com>
2020-08-19 09:41:37 +0530

    TilingProblem of recursion

---
+++ TilingProblem.cpp
@@ -0,0 +1,48 @@
+Ques: Given a wall of size 4*N and tiles of size 1*4 and 4*1. Find no. of ways to build the wall.
+
+Code:-
+
+//evolvedghost
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fi              first
+#define se              second
+#define ll              long long int
+#define pb              push_back
+#define mp              make_pair
+#define pai             pair<ll,ll>
+#define vi              vector<ll>
+#define mpi             map<ll,ll>
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inp(tc)         int tc; cin>>tc; while(tc--)
+#define fast_io         ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+
+int solve(int n) {
+	if (n <= 3) {
+		return 1;
+	}
+	int ways = solve(n - 1) + solve(n - 4);
+	return ways;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+	//for reading input from input.txt
+	freopen("input3.txt", "r", stdin);
+	//for writing output to output.txt
+	freopen("output3.txt", "w", stdout);
+#endif
+
+	fast_io;
+	inp(tc) {
+		int n;
+		cin >> n;
+		cout << solve(n) << "\n";
+	}
+}
+
